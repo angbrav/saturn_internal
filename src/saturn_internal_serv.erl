a60f6b9e2c800ae7de4f92a7d3fda227ed469466
angbrav <manuelbravogestoso@gmail.com>
2016-04-27 15:02:38 +0200

    Call to cast in server, add clean function

--- src/saturn_internal_serv.erl
+++ src/saturn_internal_serv.erl
@@ -10,6 +10,7 @@
 -export([start_link/2]).
 
 -export([handle/1,
+         restart/1,
          handle/2]).
 
 %% ------------------------------------------------------------------
@@ -39,10 +40,13 @@ start_link(Nodes, MyId) ->
     end.
 
 handle(Message) ->
-    gen_server:call(?MODULE, Message, infinity).
+    gen_server:cast(?MODULE, Message).
 
 handle(MyId, Message) ->
-    gen_server:call({global, reg_name(MyId)}, Message, infinity).
+    gen_server:cast({global, reg_name(MyId)}, Message).
+
+restart(MyId) ->
+    gen_server:call({global, reg_name(MyId)}, restart).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -59,7 +63,7 @@ init([Nodes, MyId]) ->
                           end, dict:new(), dict:to_list(Delays0)),
     {ok, #state{queues=Queues, myid=MyId, busy=Busy, delays=Delays1}}.
 
-handle_call({new_stream, Stream, IdSender}, _From, S0=#state{queues=Queues0, busy=Busy0, delays=Delays, myid=MyId}) ->
+handle_cast({new_stream, Stream, IdSender}, S0=#state{queues=Queues0, busy=Busy0, delays=Delays, myid=MyId}) ->
     Queues1 = lists:foldl(fun(Label, Acc0) ->
                             BKey = Label#label.bkey,
                             lists:foldl(fun(Node, Acc1) ->
@@ -95,13 +99,22 @@ handle_call({new_stream, Stream, IdSender}, _From, S0=#state{queues=Queues0, bus
                                             {dict:store(Node, OldQueue, Acc1), dict:store(Node, true, Acc2)}
                                     end
                                    end, {dict:new(), dict:new()}, dict:fetch_keys(Queues1)),
-    {reply, ok, S0#state{queues=Queues2, busy=Busy1}};
-
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+    {noreply, S0#state{queues=Queues2, busy=Busy1}};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
+
+handle_call(restart, _From, S0=#state{busy=Busy0, queues=Queues0}) ->
+    Queues1 = lists:foldl(fun({Node, _}, Acc) ->
+                            dict:store(Node, queue:new(), Acc)
+                          end, dict:new(), dict:to_list(Queues0)),
+    Busy1 = lists:foldl(fun({Node, _}, Acc) ->
+                            dict:store(Node, false, Acc)
+                        end, dict:new(), dict:to_list(Busy0)),
+    {reply, ok, S0#state{queues=Queues1, busy=Busy1}};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 
 handle_info({deliver_labels, Node}, S0=#state{queues=Queues0, busy=Busy0, myid=MyId}) ->
     {NewQueue, NewPending} = deliver_labels(dict:fetch(Node, Queues0), Node, MyId, []),
@@ -147,7 +160,7 @@ propagate_stream(Node, Stream, MyId) ->
             case groups_manager_serv:is_leaf(Node) of
                 {ok, true} ->
                     ServerName = list_to_atom(integer_to_list(Node) ++ atom_to_list(saturn_leaf_converger)),
-                    gen_server:call({global, ServerName}, {new_stream, Stream, MyId}, infinity);
+                    gen_server:cast({global, ServerName}, {new_stream, Stream, MyId});
                 {ok, false} ->
                     saturn_internal_serv:handle(Node, {new_stream, Stream, MyId})
             end;
